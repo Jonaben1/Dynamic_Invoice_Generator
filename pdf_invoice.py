e8661e505ba78bfc711c12523076a324946124c0
Jonathan Okah <jonaben7@gmail.com>
2023-01-17 19:42:57 +0100

    Initial commit

--- pdf_invoice.py
+++ pdf_invoice.py
@@ -188,12 +188,12 @@ def show_pdf():
     displays pdf gnerated and downloads it when prompted
     """
     # to display the pdf
-    with open('sample.pdf', 'rb') as f:
+    with open('./sample.pdf', 'rb') as f:
         base64_pdf = b64encode(f.read()).decode('utf-8')
     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
     # to dowload the pdf
-    with open('sample.pdf', 'rb') as file:
+    with open('./sample.pdf', 'rb') as file:
         st.download_button(label='Download Invoice', data=file, file_name='receipt.pdf') # defaults to mime='application/octet-stream'
 
 
